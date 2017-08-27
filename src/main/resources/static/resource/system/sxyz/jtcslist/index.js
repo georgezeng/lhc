79d522374133ad4a14e2dbb14118b7e77a8f1e9d
GeorgeZeng <ZZeng@bglcorp.com.au>
2017-08-27 17:00:30 +0800

    no message

--- src/main/resources/static/resource/system/sxyz/jtcslist/index.js
+++ src/main/resources/static/resource/system/sxyz/jtcslist/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	createCSList("/mvc/yz/listSXJTCSYZ", ["shu", "niu", "hu", "tu", "long", "she", "ma", "yang", "hou", "ji", "gou", "zhu"]);
+	createCSList("/mvc/yz/listSXJTCSYZ", ["shu", "niu", "hu", "tu", "long", "she", "ma", "yang", "hou", "ji", "gou", "zhu"], function(item) {
+		return item.currentSx.name.toLowerCase();
+	});
 });
 
 
